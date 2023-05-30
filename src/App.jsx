669f3e7488d34e1a9cc27ef243cbdb05f21e69c7
Yulman <yulmanbryan009@hotmail.com>
2023-05-29 19:38:06 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Cards from './components/Cards'
+
+function App() {
+
+  return (
+    <>
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+    <div className='App'>
+      <Cards/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cards.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const Cards = () => {
+  return (  
+    <div>Hola soy una Card</div>
+  )
+}
+
+export default Cards
